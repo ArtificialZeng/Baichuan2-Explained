34b9f07236091a524bde74bef78d71130abbda1d
Doc.Artificial <2119516028@qq.com>
2023-09-09 13:44:18 +0800

    Update web_demo.py

--- web_demo.py
+++ web_demo.py
@@ -1,16 +1,18 @@
-import json
-import torch
-import streamlit as st
-from transformers import AutoModelForCausalLM, AutoTokenizer
-from transformers.generation.utils import GenerationConfig
+# 导入streamlit库
+import streamlit as st  
+# 从transformers库中导入用于因果语言模型的自动模型和自动分词器
+from transformers import AutoModelForCausalLM, AutoTokenizer  
+# 从transformers库中导入生成配置工具
+from transformers.generation.utils import GenerationConfig  
 
+# 设置Streamlit页面的标题
+st.set_page_config(page_title="Baichuan 2")  
+st.title("Baichuan 2")  
 
-st.set_page_config(page_title="Baichuan 2")
-st.title("Baichuan 2")
-
-
-@st.cache_resource
+# 使用@st.cache_resource装饰器缓存该函数的结果
+@st.cache_resource  
 def init_model():
+    # 从预训练模型"baichuan-inc/Baichuan2-13B-Chat"加载模型
     model = AutoModelForCausalLM.from_pretrained(
         "baichuan-inc/Baichuan2-13B-Chat",
         torch_dtype=torch.float16,
@@ -20,6 +22,7 @@ def init_model():
     model.generation_config = GenerationConfig.from_pretrained(
         "baichuan-inc/Baichuan2-13B-Chat"
     )
+    # 从预训练模型"baichuan-inc/Baichuan2-13B-Chat"加载分词器
     tokenizer = AutoTokenizer.from_pretrained(
         "baichuan-inc/Baichuan2-13B-Chat",
         use_fast=False,
@@ -27,15 +30,16 @@ def init_model():
     )
     return model, tokenizer
 
-
+# 定义了一个清空会话历史的方法
 def clear_chat_history():
     del st.session_state.messages
 
-
+# 初始化或展示聊天历史
 def init_chat_history():
+    # 发送问候消息
     with st.chat_message("assistant", avatar='🤖'):
         st.markdown("您好，我是百川大模型，很高兴为您服务🥰")
-
+    # 展示聊天记录
     if "messages" in st.session_state:
         for message in st.session_state.messages:
             avatar = '🧑‍💻' if message["role"] == "user" else '🤖'
@@ -46,11 +50,14 @@ def init_chat_history():
 
     return st.session_state.messages
 
-
+# 定义应用的主要逻辑
 def main():
+    # 初始化模型和分词器
     model, tokenizer = init_model()
+    # 初始化或获取聊天历史
     messages = init_chat_history()
 
+    # 处理用户输入
     if prompt := st.chat_input("Shift + Enter 换行, Enter 发送"):
         with st.chat_message("user", avatar='🧑‍💻'):
             st.markdown(prompt)
@@ -65,8 +72,9 @@ def main():
         messages.append({"role": "assistant", "content": response})
         print(json.dumps(messages, ensure_ascii=False), flush=True)
 
+        # 提供一个按钮供用户点击，以清空对话历史
         st.button("清空对话", on_click=clear_chat_history)
 
-
+# 当脚本作为主程序运行时，执行main函数
 if __name__ == "__main__":
     main()
